37ee412c2a53813f4246020c9c50c51b28f294a6
Alejandro <alejandro.lozano@iest.edu.mx>
2025-08-29 16:51:13 -0700

    Initial Commit

---
+++ Actividad02/Actividad02App.swift
@@ -0,0 +1,17 @@
+//
+//  Actividad02App.swift
+//  Actividad02
+//
+//  Created by win603 on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Actividad02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actividad02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Actividad02
+//
+//  Created by win603 on 29/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
